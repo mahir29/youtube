2b18ff5b0939867f4f00725ddda89c0239e12a9e
mahir29 <mahirhardwar@gmail.com>
2020-09-22 21:05:57 +0530

    Updates

---
+++ precache-manifest.cfc67c5355a6f5805a3a76986a0675e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbabaf0902901136f48eb6669221ff2c",
+    "url": "/youtube/index.html"
+  },
+  {
+    "revision": "dfa714f9ddf58d7b4992",
+    "url": "/youtube/static/js/2.9c20bcac.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/youtube/static/js/2.9c20bcac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "352ff18b81babcf493f7",
+    "url": "/youtube/static/js/main.c3ec2467.chunk.js"
+  },
+  {
+    "revision": "6dd21280866e26dec379",
+    "url": "/youtube/static/js/runtime-main.2d0bda05.js"
+  }
+]);
